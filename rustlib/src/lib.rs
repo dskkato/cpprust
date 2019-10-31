abc3f517e363d3e37b50293d98abe9706594d311
Daisuke Kato <kato.daisuke429@gmail.com>
2019-11-01 06:27:40 +0900

    apply formatter

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -3,19 +3,17 @@ use std::thread;
 extern crate ndarray;
 use ndarray::prelude::*;
 
-fn mul_test(v:i32, s: usize) -> i32 {
-    let a = array![4,5,6];
+fn mul_test(v: i32, s: usize) -> i32 {
+    let a = array![4, 5, 6];
     let vv = Array::from(vec![v; s]);
     a.dot(&vv)
 }
 
 #[no_mangle]
 pub extern "C" fn hello(v: i32) -> i32 {
-    let handler = thread::spawn(move || {
-        mul_test(v, v as usize)
-    });
+    let handler = thread::spawn(move || mul_test(v, v as usize));
 
-    match handler.join(){
+    match handler.join() {
         Ok(v) => v,
         Err(_) => -1,
     }
@@ -24,11 +22,11 @@ pub extern "C" fn hello(v: i32) -> i32 {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_hello(){
+    fn test_hello() {
         assert_eq!(crate::hello(3), 45);
     }
     #[test]
-    fn test_panic(){
+    fn test_panic() {
         assert_eq!(crate::hello(2), -1);
     }
 }
